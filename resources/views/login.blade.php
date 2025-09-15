ffebcec7b227895dc51731cc4c79edb90220259e
kobieman <145900774+kobieman@users.noreply.github.com>
2025-09-15 20:18:34 +0800

    update again the css
    improve a little bit on frontend

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,9 +1,11 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{url('css/app.css')}}">
+    <link rel="stylesheet" href="{{ url('css/app.css') }}">
+    <title>Login | Dr. Romel Cruz Hospital</title>
 </head>
 <body>
     <div id="errorPopup" class="popup">
@@ -39,25 +41,51 @@
     <div class="container">
         <div class="left">
             <div class="formbox">
-                <form action="/login" method="POST">
+                <h3 class="main-title">DR ROMEL CRUZ HOSPITAL</h3>
+                <h4>Sign in to your hospital account</h4>
+                <p class="welcome">Please enter your details</p>
+                <form action="/login" method="POST" autocomplete="off">
                     @csrf
-                    <h3 class="hp">Dr. Romel Cruz Hospital</h3>
-                    <h4>Sign in to your account</h4>
-                    <p class="welcome">Please enter your details</p>
-                    <h2 class="mail">Email</h2>
-                    <input type="text" placeholder = "Enter your email address" name = "loginemail" value="{{ old('loginemail') }}">
-                    <h2 class="pass">Password</h2>
-                    <input type="password" placeholder = "Enter your password" name = "loginpassword"><br>
-                    <button type="submit">Log In</button><br>
-                    <a href="{{ url('/forgot-password') }}" style="color: #1a4931; font-weight: bold;">Forgot Password</a>
-                    <p style="margin-top: 15px; font-size: 0.9em; color: #555; font-style: italic;">
-                        Need an account? Please contact the hospital administrator
-                    </p>
+                    <div style="text-align:left; margin-bottom: 18px;">
+                        <label for="loginemail" style="color:#1a4931; font-weight:600; font-size:1rem; margin-bottom:4px; display:block;">
+                            Email
+                        </label>
+                        <input 
+                            type="text" 
+                            id="loginemail"
+                            name="loginemail" 
+                            placeholder="Email Address" 
+                            value="{{ old('loginemail') }}" 
+                            required 
+                            autofocus
+                        >
+                    </div>
+                    <div style="text-align:left; margin-bottom: 10px;">
+                        <label for="loginpassword" style="color:#1a4931; font-weight:600; font-size:1rem; margin-bottom:4px; display:block;">
+                            Password
+                        </label>
+                        <input 
+                            type="password" 
+                            id="loginpassword"
+                            name="loginpassword" 
+                            placeholder="Password" 
+                            required
+                        >
+                    </div>
+                    <button type="submit">Log In</button>
                 </form>
-           </div>
+                <div class="social-login">
+                    <p>
+                        <a href="{{ url('/forgot-password') }}">Forgot Password?</a>
+                    </p>
+                </div>
+                <p style="margin-top: 15px; font-size: 0.9em; color: #555; font-style: italic;">
+                    Need an account? Please contact the hospital administrator
+                </p>
+            </div>
         </div>
         <div class="right">
-            <img src="{{ asset('img/romelcruz1.JPG')}}" alt="">
+            <img src="{{ asset('img/hospital_logo.png') }}" alt="Hospital Logo" class="hospital-logo">
         </div>
     </div>
 </body>
