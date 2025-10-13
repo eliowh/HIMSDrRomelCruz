8172128ddba7376783528edd5b8b579b50f7e72a
johnaurenceagpayo20 <johnlaurencemagpayo20@gmail.com>
2025-10-13 10:13:38 +0800

    fixchatforDoctorsOnly

--- routes/web.php
+++ routes/web.php
@@ -330,7 +330,7 @@ Route::middleware(['auth', 'role:nurse'])->group(function () {
     // Allow nurses to view analysis PDFs (same as doctors)
     Route::get('/nurse/results/{labOrderId}/analysis-pdf', [LabOrderController::class, 'generateAnalysisPdf'])->name('nurse.results.analysis-pdf');
     
-    // Chat/Messaging System for Nurses
+    // Chat/Messaging System for Nurses (No group chat creation - only patient chats)
     Route::get('/nurse/chat', [App\Http\Controllers\ChatController::class, 'index'])->name('nurse.chat.index');
     Route::get('/nurse/chat/{id}', [App\Http\Controllers\ChatController::class, 'show'])->name('nurse.chat.show');
     Route::post('/nurse/chat/create-for-patient', [App\Http\Controllers\ChatController::class, 'createOrGetForPatient'])->name('nurse.chat.createForPatient');
@@ -340,7 +340,7 @@ Route::middleware(['auth', 'role:nurse'])->group(function () {
     Route::get('/nurse/chat/{id}/messages', [App\Http\Controllers\ChatController::class, 'getMessages'])->name('nurse.chat.getMessages');
     Route::patch('/nurse/chat/{id}/archive', [App\Http\Controllers\ChatController::class, 'archive'])->name('nurse.chat.archive');
     Route::get('/nurse/chat/attachment/{id}/download', [App\Http\Controllers\ChatController::class, 'downloadAttachment'])->name('nurse.chat.downloadAttachment');
-    Route::post('/nurse/chat/create-role-group', [App\Http\Controllers\ChatController::class, 'createRoleGroup'])->name('nurse.chat.createRoleGroup');
+    // Note: Group chat creation removed - only doctors can create group chats
     
     // Medicine request history for nurses
     Route::get('/nurse/medicine-request-history', [PharmacyController::class, 'nurseRequestHistory'])->name('nurse.medicine.request.history');
